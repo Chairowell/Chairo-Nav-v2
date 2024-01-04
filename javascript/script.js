f677ef5d3655c2e91190b793a8822858273200c0
是一只栗子啊 <chairowell@gmail.com>
2024-01-04 20:36:39 +0800

    更新 script.js

--- javascript/script.js
+++ javascript/script.js
@@ -367,7 +367,7 @@ if(window.location.hash == "#R18-CLOSE"){
 }
 document.addEventListener('DOMContentLoaded', function () {
     
-    fetch('https://raw.gitmirror.com/Chairowell/Pictures-BED/main/festival/newyear/poster.json')
+    fetch('https://raw.gitmirror.com/Chairowell/Pictures-BED/main/2024-01-C/poster.json')
     .then((response) => response.json())
     .then((json) => {
         const data = json;
