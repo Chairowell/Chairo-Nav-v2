e2297b6c0aaf1bdd295a7cb6c5f811bc7ced774e
是一只栗子啊 <chairowell@gmail.com>
2024-02-02 15:34:41 +0800

    upload

--- javascript/script.js
+++ javascript/script.js
@@ -400,7 +400,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 linkBox = `
                     ${linkBox}
                     <div class="link-box" data-umami-event="${link}">
-                        <a href="${link}">
+                        <a href="${link}" rel="nofollow noopener noreferrer">
                             <div class="link-icon" style="background-image:url(https://api.chairo.cc/get.php?url=${link});"></div>
                             <div class="link-name">${name}</div>
                             <div class="link-explain">${explain}</div>
